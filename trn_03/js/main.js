ca7799c9e4b66d31fbd31348d309f127616fbbc6
wisberry <wisberry2022@gmail.com>
2022-08-08 17:46:49 +0900

    08/08 upload

--- trn_03/js/main.js
+++ trn_03/js/main.js
@@ -1,8 +1,42 @@
 $(function () {
-  $('.main_slider').slick({
+
+  function closeBanner() {
+    // $('.TopBanner').toggleClass('on');
+    $('.TopBanner').slideToggle('on');
+    // document.querySelector('.TopBanner').classList.toggle('on');
+  }
+
+  $('.TopBanner i').on('click', closeBanner);
+
+  $('.MainSlider').slick({
     arrows: false,
     dots: true,
     autoplay: true,
-
   });
+
+  $('.ProductSlider').slick({
+    arrows: false,
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 1230,
+        settings: {
+          slidesToShow: 1,
+          dots: true
+        }
+      },
+    ]
+  });
+
+  function toggleClass() {
+    $('.pop li').toggleClass('on');
+    $('.Footer .popup').toggleClass('on');
+  }
+
+  $('.pop li').on('click', toggleClass);
+
+  $('.popup i').on('click', function () {
+    $('.popup').removeClass('on');
+  })
+
 })
