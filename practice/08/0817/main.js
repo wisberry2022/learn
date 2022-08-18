42f159450a132da22b679e93e97bb341047dad59
wisberry <wisberry2022@gmail.com>
2022-08-18 17:45:31 +0900

    08/18 upload

--- practice/08/0817/main.js
+++ practice/08/0817/main.js
@@ -1,43 +1,122 @@
 window.addEventListener('DOMContentLoaded', function () {
 
+  // 계산기 객체
+  const CALC_OBJ = {
+    name: 'calc',
+    acc: '',
+    window_acc: '',
+    result: 0,
+    expression: new Array(),
+    __show: function () {
+      console.log(this.name, this.acc, this.result, this.expression);
+    },
+
+    __numClick: function (value) {
+      this.acc += value;
+    },
+
+    __windowNumClick: function (value) {
+      this.window_acc += value;
+    },
+
+    __calculate: function () {
+      this.expression.forEach(function (elm, idx, arr) {
+        if (typeof (elm) == 'number') {
+          if (!(idx)) {
+            console.log(this.result);
+            this.result += elm;
+          } else {
+            if (arr[idx - 1] == '+') {
+              this.result += elm;
+            } else if (arr[idx - 1] == '-') {
+              this.result -= elm;
+            } else if (arr[idx - 1] == 'X') {
+              this.result *= elm;
+            } else if (arr[idx - 1] == '/') {
+              this.result /= elm;
+            } else if (arr[idx - 1] == '%') {
+              this.result %= elm;
+            }
+          }
+        }
+      }, this)
+    },
+
+    numClick: function (value) {
+      this.acc += value;
+      this.__show();
+    },
+
+    operandClick: function (value) {
+      this.expression.push(parseInt(this.acc));
+      this.expression.push(value);
+      this.acc = '';
+    },
+
+    resultClick: function () {
+      this.expression.push(parseInt(this.acc));
+      this.__calculate();
+      this.__show();
+    },
+
+    clickShow: function (value, showObj) {
+      this.__windowNumClick(value);
+      showObj.innerText = this.window_acc;
+    },
+
+
+    deleteClick: function (delete_obj) {
+      let result = delete_obj.innerText.split('');
+      let temp = result.splice(-1, 1);
+      delete_obj.innerText = result.join('');
+      let w_result = this.window_acc.split('');
+      let w_temp = w_result.splice(-1, 1);
+      this.window_acc = w_result.join('');
+      let a_result = this.acc.split('');
+      let a_temp = a_result.splice(-1, 1);
+      this.acc = a_result.join('');
+    },
+
+    clearClick: function (...clearObj) {
+      clearObj.forEach(function (elm) { elm.innerText = ''; });
+      this.window_acc = '';
+      this.acc = '';
+      this.expression = [];
+    },
+  };
+
   const BTN = document.querySelector('.btn_box');
   const RESULT_SHOW = document.querySelector('.input_window .result');
   const CURRENT = document.querySelector('.input_window .current');
 
-  let result = 0;
-  let acc = '';
-  let operand = '';
-  let idx = 0;
+  let calc = CALC_OBJ;
 
   BTN.addEventListener('click', function (event) {
+    const currentClick = event.target;
+    // 계산기 패드를 눌렀을 때
     if (event.target != event.currentTarget) {
-
-      let clickNum = event.target.textContent;
-
-      // clear, delete 버튼을 누르지 않았을 때
-      if (!(event.target.classList.contains('func'))) {
-        CURRENT.innerText += clickNum;
-        // 숫자 패드를 눌렀을 때
-        if ((event.target.classList.contains('number'))) {
-          RESULT_SHOW.innerText += clickNum;
-          acc += clickNum;
-        }
-      } else { // 연산자 패드를 눌렀을 때
-        RESULT_SHOW.innerText = '';
-        operand = clickNum;
-        if (!(idx)) {
-          result += parseInt(acc);
-          acc = '';
+      // 숫자 패드를 눌렀을 때
+      if (currentClick.classList.contains('number')) {
+        calc.clickShow(currentClick.innerText, CURRENT);
+        calc.numClick(currentClick.innerText);
+      }// 연산자 패드를 눌렀을 때
+      else if (currentClick.classList.contains('operand')) {
+        calc.clickShow(currentClick.innerText, CURRENT);
+        if (currentClick.innerText == '=') {
+          calc.resultClick();
         } else {
-
+          calc.operandClick(currentClick.innerText);
         }
-        idx += 1;
-      }
+
+      } else // func 패드를 눌렀을 때 {
+        // DELETE 패드를 눌렀을 때
+        if (currentClick.innerText == 'DELETE') {
+          calc.deleteClick(CURRENT);
+        }
+        // CLEAR 패드를 눌렀을 때
+        else {
+          calc.clearClick(CURRENT, RESULT_SHOW);
+        }
     }
-    console.log(result);
-
-  }
-
-  })
-
+  });
 });
--- trn_04/js/main.js
+++ trn_04/js/main.js
@@ -5,6 +5,10 @@ window.addEventListener('DOMContentLoaded', () => {
     sct > 0
       ? document.querySelector('#Header').classList.add('on')
       : document.querySelector('#Header').classList.remove('on')
+
+    sct > 500
+      ? document.querySelector('.to_top').classList.add('on')
+      : document.querySelector('.to_top').classList.remove('on')
   });
 
   const MainSlider = new Swiper('.swiper', {
@@ -30,10 +34,18 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   });
 
+  document.querySelector('.to_top').addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  })
 
+  document.querySelector('.mopen').addEventListener('click', (e) => {
+    const TG = e.currentTarget;
+    TG.classList.toggle('on');
+    document.querySelector('#Header').classList.toggle('ox');
+  });
 
-
-
-
-
+  document.querySelector('#Header').addEventListener('wheel', (e) => {
+    e.stopPropagation(); // 이벤트 자체를 막음
+    e.preventDefault();
+  })
 })
